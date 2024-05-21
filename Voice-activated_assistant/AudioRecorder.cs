3061dc5762a2697a42c4f9b5ee65506648486ca7
sam <samliao1@auforce.com>
2024-05-21 18:01:29 +0800

    ✨ feat: 簡單的錄音與傳換為文字

---
+++ Voice-activated_assistant/AudioRecorder.cs
@@ -0,0 +1,100 @@
+﻿using NAudio.Wave;
+
+namespace Voice_activated_assistant
+{
+    /// <summary>
+    /// 使用 NAudio 錄音
+    /// </summary>
+    public class AudioRecorder
+    {
+        private WaveInEvent? waveSource = null;
+        private WaveFileWriter? waveFile = null;
+        private bool isRecording = false;
+        private readonly double threshold = 0.001; // 設定聲音偵測的閾值
+        public string outputFilePath = "";
+
+        /// <summary>
+        /// 開始錄音
+        /// </summary>
+        /// <param name="outputFilePath"></param>
+        public void StartRecording()
+        {
+            this.outputFilePath = DateTime.Now.ToString("yyyy_MM_DD_HH_mm_ss_fff") + ".wav";
+
+            waveSource = new WaveInEvent
+            {
+                WaveFormat = new WaveFormat(16000, 1) // 設置錄音的格式，這裡為 CD 質量
+            };
+
+            waveSource.DataAvailable += new EventHandler<WaveInEventArgs>(WaveSource_DataAvailable);
+            waveSource.RecordingStopped += new EventHandler<StoppedEventArgs>(WaveSource_RecordingStopped);
+
+            waveSource.StartRecording();
+        }
+
+        /// <summary>
+        /// 可用資料
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WaveSource_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            // 計算音頻數據的振幅
+            float amplitude = 0;
+            for (int index = 0; index < e.BytesRecorded; index += 2)
+            {
+                short sample = BitConverter.ToInt16(e.Buffer, index);
+                amplitude += Math.Abs(sample / 32768f);
+            }
+            amplitude /= (e.BytesRecorded / 2);
+
+            // 如果振幅超過閾值，則開始錄音
+            if (amplitude > threshold && !isRecording)
+            {
+                waveFile = new WaveFileWriter(this.outputFilePath, waveSource?.WaveFormat);
+                isRecording = true;
+            }
+
+            // 如果正在錄音，則將音頻數據寫入檔案
+            if (isRecording)
+            {
+                waveFile?.Write(e.Buffer, 0, e.BytesRecorded);
+                waveFile?.Flush();
+            }
+        }
+
+        /// <summary>
+        /// 停止錄音
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WaveSource_RecordingStopped(object sender, StoppedEventArgs e)
+        {
+            waveSource?.Dispose();
+            waveSource = null;
+
+            waveFile?.Dispose();
+            waveFile = null;
+
+            isRecording = false;
+        }
+
+        /// <summary>
+        /// 停止錄音
+        /// </summary>
+        public void StopRecording()
+        {
+            waveSource?.StopRecording();
+        }
+
+        /// <summary>
+        /// 判斷是否在錄音中
+        /// </summary>
+        /// <returns></returns>
+        public bool IsRecording()
+        {
+            return isRecording;
+        }
+
+    }
+}
---
+++ Voice-activated_assistant/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Voice_activated_assistant;
+using Whisper.net;
+using Whisper.net.Ggml;
+
+// 指定輸出為 UTF8
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+string currentDirectory = Environment.CurrentDirectory;
+Console.WriteLine($"目前的工作目錄: {currentDirectory}");
+
+var modelName = "ggml-base-q5_1.bin";
+if (File.Exists(modelName))
+{
+    Console.WriteLine($"{modelName} 檔案已經存在，不須下載模型");
+}
+else
+{
+    Console.WriteLine($"{modelName} 檔案不存在，準備下載檔案");
+
+    using var modelStream = await WhisperGgmlDownloader.GetGgmlModelAsync(GgmlType.Base);
+    using var fileWriter = File.OpenWrite(modelName);
+    await modelStream.CopyToAsync(fileWriter);
+}
+
+using var whisperFactory = WhisperFactory.FromPath(modelName);
+using var processor = whisperFactory.CreateBuilder()
+    .WithLanguage("auto")
+    .Build();
+
+var recorder = new AudioRecorder();
+while (true)
+{
+    if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape)
+    {
+        break;
+    }
+
+    recorder.StartRecording();
+    Thread.Sleep(10000); // 等待一段時間，例如10秒
+    recorder.StopRecording();
+
+    while (recorder.IsRecording())
+    {
+        Thread.Sleep(10); // 等待一段時間，例如10秒
+    }
+
+    // 使用當前的時間戳來創建一個唯一的檔案名稱
+    if (File.Exists(recorder.outputFilePath))
+    {
+        //Console.WriteLine($"{recorder.outputFilePath} 有錄音檔案");
+
+        using var fileStream = File.OpenRead(recorder.outputFilePath);
+        await foreach (var result in processor.ProcessAsync(fileStream))
+        {
+            if(result.Text != "Inaudible")
+            {
+                Console.WriteLine($"{result.Start}->{result.End}: {result.Text}");
+            }
+        }
+    }    
+}
+
+Console.WriteLine("程式已結束!");
